0a728641c99a3be33d6a56d9dbfa17c7b4e49728
arasadaf <arasadaf012@gmail.com>
2026-05-25 13:54:24 +0530

    chore: ignore node_modules and fix line endings

---
+++ usermgt/user-management-app/frontend/src/components/Home.jsx
@@ -0,0 +1,19 @@
+/*import React from 'react'
+
+function Home() {
+    return (
+    <div>Home</div>
+    )
+}
+
+export default Home*/
+function Home(){
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold">Blog Platform</h1>
+      <p className="mt-4 text-gray-600">Welcome to the blog application</p>
+    </div>
+  )
+}
+
+export default Home
